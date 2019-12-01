fc902763c94eae369c2649e29cdb65dd0a5dfc4f
imkimheejoo <jujudamdadi@naver.com>
2019-12-01 20:25:21 +0900

    feat: props,emit 예제 구현

--- cue-cli/src/App.vue
+++ cue-cli/src/App.vue
@@ -1,15 +1,28 @@
 <template>
   <div>
-    {{str}}
+    <!-- <app-header v:bind:프롭스 속성이름(하위) = "상위 컴포넌트의 데이터 이름"></app-header> -->
+    <!-- v-on:위에서 보낸 이벤트 이름="실행시킬 함수 이름" -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
   <!-- template에는 무조건 하나의 요소만 있어야 함 -> 또다른 태그 추가 할 수 없음! (그 안에다 정의하는건 상관 없음) -->
 </template>
 
 <script>
+// AppHeader.vue에 있는 값들이 AppHeader라는 변수에 담김!
+import AppHeader from './components/AppHeader.vue';
+
 export default {
   data: function() {
     return {
-      str: 'hi'
+      str: 'Header'
+    }
+  },
+  components: {
+    'app-header': AppHeader
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'hi'
     }
   }
 }
---
+++ cue-cli/src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+      <h1>{{ propsdata }}</h1>
+      <!-- v-on:click="실행할 메서드 이름" -->
+      <button v-on:click="sendEvent">send</button>
+  </header>
+</template>
+
+<script>
+export default {
+    props: ['propsdata'],
+    methods: {
+        sendEvent:function() {
+            //emit 안에 이벤트 이름을 적음 (App에 이벤트를 보냄) -> app-header 태그가 이것을 받음
+            this.$emit('renew')
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
